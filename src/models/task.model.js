acdd6f3da3e96a23f41d8b07f72be1c5ad31dbea
Samriddhiy <samriddhi4005@gmail.com>
2025-08-08 01:01:45 +0530

    model added

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -15,8 +15,24 @@ const taskSchema = new mongoose.Schema({
     createdBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-    },
-} , {
+    }, 
+
+    ratings: [
+        {
+            user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+            },
+            rating: {
+            type: Number,
+            min: 1,
+            max: 5
+            }
+        }
+    ],
+
+}, 
+{
     timestamps: true,
 })
 
