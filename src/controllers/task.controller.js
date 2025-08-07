be0042942bf35c332c66830072143eef74adda5b
Samriddhiy <samriddhi4005@gmail.com>
2025-08-07 23:25:11 +0530

    update and delete task

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -24,9 +24,60 @@ const createTask = async (req, res ) => {
     } catch (error) {
         return res.status(500).json({message: "Error in creating task." , error: error.message});
     }
-} 
+}
+
+const updateTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description, dueDate } = req.body;
+
+        if (!id) {
+            return res.status(404).json({ message: "Task Id is required."});
+        }
+
+        const updatedTask = await Task.findByIdAndUpdate(
+            id,
+            { title, description, dueDate },
+            { new: true }
+        );
+
+        if (!updatedTask) {
+            return res.status(404).json({ message: "Task not found." });
+        }
+
+
+
+        return res.status(200).json({message: " Task updated successfully.", updatedTask})
+        
+    } catch (error) {
+        return res.status(402).json({ message: " Error in updating." , error: error.message});
+    }
+}
+
+const deleteTask = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json({ message: "Task Id is required." });
+        }
+
+        const deletedTask = await Task.findByIdAndDelete(id);
+
+        if (!deletedTask) {
+            return res.status(404).json({ message: "Task not found." });
+        }
+        
+        return res.status(200).json({message: " Task deleted successfully." , deletedTask})
+    } catch (error) {
+        return res.status(400).json({ message: "Error in deleting the task." , error: error.message});
+    }
+}
 
 export {
     createTask,
+    updateTask,
+    deleteTask
 
 }
